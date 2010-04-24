bb99fec3054f5039f17efeaa9e8a407891a47038
zai <zir.echo@gmail.com>
2010-04-24 17:24:42 +0800

    directory structure change

---
+++ index.js
@@ -0,0 +1,5 @@
+// exports modules
+exports.core = require("./genji/core");
+exports.crypto = require("./genji/crypto");
+exports.utils = require("./genji/utils");
+exports.web = require("./genji/web");
