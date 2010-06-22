4a86c9e7dcdb895b8706b9b2745a5ee56da174d0
Senmiao Liu <zir.echo@gmail.com>
2010-06-23 00:55:51 +0800

    update test

--- test/spec/core/base.js
+++ test/spec/core/base.js
@@ -1,33 +1,43 @@
-var Base = require('genji/core/base').Base;
+describe('Base', function() {
+    var Base = require('genji/core/base').Base;
+    var Class, ClassPlus, ClassPlusPlus, Class4
+    , klass, klassPlus, klassPlusPlus, klass4;
 
-var Class = Base({
-    init: function(name) {
-        this.name = name;
-    },
+    beforeEach(function() {
+        Class = Base(function(name) {
+            this.name = name;
+        });
 
-    getName: function() {
-        return this.name;
-    },
+        ClassPlus = Class({
+            init: function(name, age) {
+                this._super(name+1);
+                this.age = age;
+            }
+        });
 
-    say: function() {
-        return "I'm the first one";
-    }
-});
+        ClassPlusPlus = ClassPlus({
+            getName: function() {
+               return this.name;
+            },
+            getAge:  function() {
+               return this.age;
+            }
+        });
 
-var ClassPlus = Class({
-    setName: function(name) {
-        this.name = name;
-    },
+        Class4 = ClassPlusPlus({
+            init: function(name, age) {
+                this._super(name, age);
+            }
+        });
 
-    say: function() {
-        return this._super() + ", while I'm the second one";
-    }
-});
+        klass = new Class('class');
+        klassPlus = new ClassPlus('classPlus', 18);
+        klassPlusPlus = new ClassPlusPlus('classPlusPlus', 26);
+        klass4 = new Class4('class4', 14);
+    });
 
-var klass = new Class('A class');
-var klassPlus = new ClassPlus('B class');
+            
 
-describe('Base', function () {
     describe('inheritance', function() {
         it('should be an instance of their respective class, the same for their constructor', function() {
             expect(klass.constructor).toBe(Class);
@@ -35,29 +45,95 @@ describe('Base', function () {
             expect(klassPlus.constructor).toBe(ClassPlus);
             expect(klassPlus instanceof Class).toEqual(true);
             expect(klassPlus instanceof ClassPlus).toEqual(true);
+            expect(klassPlusPlus.constructor).toBe(ClassPlusPlus);
+            expect(klassPlusPlus instanceof Class).toEqual(true);
+            expect(klassPlusPlus instanceof ClassPlus).toEqual(true);
+            expect(klassPlusPlus instanceof ClassPlusPlus).toEqual(true);
+        });
+        it('should not affect parent class and instances', function() {
+            expect(ClassPlus.prototype.getAge).toBeUndefined();
+            expect(klassPlus.getName).toBeUndefined();
+        });
+    });
+
+    describe('property and constructor', function() {
+        it('should has properties setted in constructor', function() {
+            expect(klass.name).toEqual('class');
+            expect(klassPlus.age).toEqual(18);
+        });
+        it('should be able to set properties by calling parent\'s constructor', function() {
+            expect(klassPlus.name).toEqual('classPlus1');
+            expect(klass4.getAge()).toEqual(14);
+            expect(klass4.getName()).toEqual('class41');
+        });
+        it('', function() {});
+        it('should call parent\'s constructor if there\'s no `init` defined', function() {
+            expect(klassPlusPlus.getName()).toEqual('classPlusPlus1');
+            expect(klassPlusPlus.getAge()).toEqual(26);
+        });
+    });
+});
+
+
+describe('Class', function () {
+    var Class = require('genji/core/base').Class;
+    var MyClass, MyClassPlus, klass, klassPlus;
+
+    beforeEach(function() {
+        MyClass = Class({
+            init: function(name) {
+                this.name = name;
+            },
+            getName: function() {
+                return this.name;
+            },
+            say: function() {
+                return "I'm the first one";
+            }
+        });
+
+        MyClassPlus = MyClass({
+            setName: function(name) {
+                this.name = name;
+            },
+            say: function() {
+                return this._super() + ", while I'm the second one";
+            }
+        });
+
+        klass = new MyClass('A class');
+        klassPlus = new MyClassPlus('B class');
+    });
+    describe('inheritance', function() {
+        it('should be an instance of their respective class, the same for their constructor', function() {
+            expect(klass.constructor).toBe(MyClass);
+            expect(klass instanceof MyClass).toEqual(true);
+            expect(klassPlus.constructor).toBe(MyClassPlus);
+            expect(klassPlus instanceof MyClass).toEqual(true);
+            expect(klassPlus instanceof MyClassPlus).toEqual(true);
+        });
+        it('should not affect parent class and instances', function() {
+            expect(MyClass.prototype.setName).toBeUndefined();
+            expect(klass.setName).toBeUndefined();
         });
     });
 
     describe('defining properties', function() {
         it('should has methods', function() {
-            expect(Class.prototype.hasOwnProperty('init')).toEqual(true);
-            expect(Class.prototype.hasOwnProperty('getName')).toEqual(true);
-            expect(Class.prototype.hasOwnProperty('say')).toEqual(true);
+            expect(MyClass.prototype.hasOwnProperty('init')).toEqual(true);
+            expect(MyClass.prototype.hasOwnProperty('getName')).toEqual(true);
+            expect(MyClass.prototype.hasOwnProperty('say')).toEqual(true);
             expect(typeof klass.init).toEqual('function');
             expect(typeof klass.getName).toEqual('function');
             expect(typeof klass.say).toEqual('function');
         });
 
-        it('should be called as constructor of the class', function() {
+        it('should has properties setted by constructor of the class', function() {
             expect(klass.name).toEqual('A class');
             expect(klass.getName()).toEqual('A class');
             expect(klassPlus.getName()).toEqual('B class');
         });
 
-        it('should not affect parent class and instances', function() {
-            expect(Class.prototype.setName).toBeUndefined();
-            expect(klass.setName).toBeUndefined();
-        });
     });
     
     describe('_super and override', function() {
