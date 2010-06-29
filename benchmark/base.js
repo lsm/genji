f5ff020b4d49724625cf4731dc4b289ef23edde1
Senmiao Liu <zir.echo@gmail.com>
2010-06-29 19:20:10 +0800

    benchmark base.js

---
+++ benchmark/base.js
@@ -0,0 +1,87 @@
+var Base = require('../lib/genji/core/base').Base;
+var sys = require('sys'),
+times = 500000;
+function bm(label, fn) {
+    var start = +new Date
+    fn()
+    sys.puts('  ' + label + ' : ' + (+new Date - start) + ' ms')
+}
+
+sys.puts('\n  running ' + times + ' times\n')
+sys.puts('\n  class definition \n')
+bm('prototype', function(){
+    var n = times
+    while (n--) {
+        function User(name) {
+            this.name = name.trim()
+        }
+        function Admin(name) {
+            User.call(this, name)
+        }
+    }
+})
+
+bm('sys.inherits()', function(){
+    var n = times
+    while (n--) {
+        function User(name) {
+            this.name = name.trim()
+        }
+        function Admin(name) {
+            User.call(this, name)
+        }
+        sys.inherits(Admin, User)
+    }
+})
+
+bm('Base', function(){
+    var n = times
+    while (n--) {
+        var User = Base(function(name) {
+            this.name = name.trim()
+        });
+        var Admin = User({
+            init: function(name) {
+                this._super(name);
+            }
+        });
+    }
+})
+
+sys.puts('\n  instance creation \n')
+
+bm('prototype', function(){
+    var n = times
+    function User(name) {
+        this.name = name.trim()
+    }
+    function Admin(name) {
+        User.call(this, name)
+    }
+    while (n--) new Admin('tj')
+})
+
+bm('sys.inherits()', function(){
+    var n = times
+    function User(name) {
+        this.name = name.trim()
+    }
+    function Admin(name) {
+        User.call(this, name)
+    }
+    sys.inherits(Admin, User)
+    while (n--) new Admin('tj')
+})
+
+bm('Base', function(){
+    var n = times
+    var User = Base(function(name) {
+        this.name = name.trim()
+    });
+    var Admin = User({
+        init: function(name) {
+            this._super(name);
+        }
+    });
+    while (n--) new Admin('tj')
+})
