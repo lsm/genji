fe79ac7ef6ff69fed6e5b6b78ff31b5942a5db08
Senmiao Liu <zir.echo@gmail.com>
2010-09-05 02:40:25 +0800

    change require path

--- benchmark/base.js
+++ benchmark/base.js
@@ -1,5 +1,5 @@
 // benchmark scripts copied from Class.js (TJ Holowaychuk)
-var Base = require('../lib/genji/core').Base;
+var Base = require('../lib/genji/pattern/base');
 var sys = require('sys'),
 times = 500000;
 function bm(label, fn) {
