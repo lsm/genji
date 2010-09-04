43cf1c3f1515110399e463aed3c8424794c33a7d
Senmiao Liu <zir.echo@gmail.com>
2010-09-05 02:40:43 +0800

    example of middleware+router

--- example/helloworld/server-app.js
+++ example/helloworld/server-app.js
@@ -1,9 +1,21 @@
 var genji = require('../../lib/genji');
 
 var urls = [
-    ['/hello', function(h) {
-            h.sendHTML('Hello world\n');
-    }]
+    ['^/hello/$', function() {
+            this.sendHTML('Hello world\n');
+    }],
+    ['^/prefix/', [
+            ['a/$', function() {this.sendHTML('got a');}],
+            ['b/$', function() {this.sendHTML('got b');}]
+    ]]
 ]
 
-genji.web.router.createServer(urls, genji.web.handler.SimpleHandler)
+genji.web.startServer({
+    host: '127.0.0.1',
+    port: '8000',
+    middlewares: [
+        {name: 'error-handler'},
+        {name: 'logger'},
+        {name: 'router', urls: urls}
+    ]
+})
