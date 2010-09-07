af9d257766553a41b4516fce5d5a4ff8f5a26980
Senmiao Liu <zir.echo@gmail.com>
2010-09-07 16:34:55 +0800

    cleanup

--- test-jasmine/run_specs.js
+++
@@ -1,9 +0,0 @@
-require.paths.unshift('./jasmine-node/lib/', '../lib');
-var jasmine = require('jasmine');
-
-// this is needed for asyncSpecWait
-for(var key in jasmine) {
-  global[key] = jasmine[key];
-}
-
-jasmine.run(__dirname);
