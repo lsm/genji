fd0ff1e2d5e9d76fe5fbd00330f720a624c99b12
Senmiao Liu <zir.echo@gmail.com>
2010-09-20 02:28:32 +0800

    add tests

---
+++ test/pattern.control.js
@@ -0,0 +1,60 @@
+var c = require('genji/pattern/control'),
+Chain = c.Chain,
+chain = c.chain,
+promise = c.promise,
+fs = require('fs');
+
+module.exports = {
+    'test executing the same function against different array members': function(assert) {
+        var res = 0;
+        var c1 = chain([1, 2, 3], function(item, idx, arr, next, args) {
+            res += item;
+            next();
+        });
+        c1();
+        assert.equal(res, 6);
+    },
+
+    'test chaining functions and call with order of FIFC': function(assert) {
+        var c2 = new Chain, res = 0, order = [];
+        c2.add('fc', function(n) {
+            res += n;
+            order.push(0);
+            this.next(res);
+        });
+        c2.add('fc', function(n) {
+            res += n;
+            order.push(1);
+            return res; // == this.next(1);
+        });
+        c2.add('fc', function(n) {
+            res += n;
+            order.push(2);
+            this.next(res);
+        });
+        c2.get('fc')(1);
+        assert.equal(res, 3);
+        assert.eql(order, [0,1,2]);
+    },
+
+    'test chain name not exists': function(assert) {
+        try {
+            var c3 = new Chain;
+            c3.get('aa');
+            assert.equal(1, 2); // should not be called
+        } catch (e) {
+            assert.equal(e.message, 'Chain named aa not exists.');
+        }
+    },
+
+    'test promise': function(assert, beforeExit) {
+        var readFile = promise(fs.readFile), finished;
+        fs.readFile(__filename, function(err, data1) {
+            if (err) throw err;
+            readFile(__filename).then(function(err, data2) {
+                if (err) throw err;
+                assert.eql(data1, data2);
+            });
+        });
+    }
+}
---
+++ test/pattern.pool.js
@@ -0,0 +1,36 @@
+var Pool = require('genji/pattern/pool');
+
+
+function feedPool(num, callback) {
+    for (var i = 0; i < num; i++) {
+        callback(i);
+    }
+}
+
+module.exports = {
+    'test Pool': function(assert) {
+        var pool =  new Pool(feedPool, 10);
+        assert.equal(pool.length, 10);
+        for (var i = 0; i < 10; i++) {
+            pool.pop(function(item) {
+                assert.equal(item, i);
+                assert.equal(pool.length, 9 - i);
+            });
+        }
+        // the pool is empty, tasks will be queued
+        for (var i = 0; i < 10; i++) {
+            pool.pop(function(item) {
+                // function will not be called until something back to the pool
+                throw new Error('Catch me when '+item+' back.');
+            });
+        }
+        try {
+            pool.emit('back', 'you');
+            assert.equal(1, 'This should never be called');
+        } catch (e) {
+            assert.equal(e.message, 'Catch me when you back.');
+            // still 9 tasks in the queue
+            assert.equal(pool.queue, 9);
+        }
+    }
+}
