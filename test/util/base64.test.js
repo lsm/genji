f8123fe3107238c90f90514bf32661f80a7fba4d
Senmiao Liu <zir.echo@gmail.com>
2010-10-11 03:11:31 +0800

    utf8 characters

--- test/util/base64.test.js
+++ test/util/base64.test.js
@@ -2,7 +2,7 @@ var base64 = require('genji/util').base64;
 
 module.exports = {
     'test base64 encoding end decoding': function(assert) {
-        assert.equal(base64.encode('nihao'), 'bmloYW8=');
-        assert.equal(base64.decode('bmloYW8='), 'nihao');
+        assert.equal(base64.encode('你好'), '5L2g5aW9');
+        assert.equal(base64.decode('5L2g5aW9'), '你好');
     }
 }
