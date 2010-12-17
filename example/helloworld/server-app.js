715dd4342a1fb20628be3ec8585ccbaa276d8c21
Senmiao Liu <zir.echo@gmail.com>
2010-12-17 21:34:08 +0800

    more verbose and comments

--- example/helloworld/server-app.js
+++ example/helloworld/server-app.js
@@ -1,21 +1,40 @@
+// require the genji lib
 var genji = require('../../lib/genji');
 
+// map url to business logic
 var urls = [
-    ['^/hello/$', function() {
-            this.sendHTML('Hello world\n');
-    }],
-    ['^/prefix/', [
-            ['a/$', function() {this.sendHTML('got a');}],
-            ['b/$', function() {this.sendHTML('got b');}]
-    ]]
-]
+    // each member in the `urls` array is a mapping
+    // the simplest format would be [RegExp, Function]
+    [
+        '^/hello/$', // RegExp pattern or object that you want to match
+        function(h) { // function which handle your business
+            h.sendHTML('Hello world\n');
+        }
+    ],
+    ['^/prefix/', 
+        [// you can put an array here instead of function, which means it's a sub-url mapping
+            [
+                '^a/$', // then url use to match the pattern will become `a/` if you original url is `/prefix/a/`,
+                // so make sure to place the leading `^` in front of `a`, otherwise `/prefix/ba/` will be matched;
+                function(h) {h.sendHTML('got a');}
+            ],
+            // you can define as many sub-url mappings as you wish
+            ['^b/$', function(h) {h.sendHTML('got b');}]
+    ]],
+    ['/', function(h) {
+            h.sendHTML('<a href="/hello/">Hello world</a>');
+    }]
+];
 
-genji.web.startServer({
+var settings = {
     host: '127.0.0.1',
     port: '8000',
     middlewares: [
+        // your can use buildin middlewares by just define the `name` property
         {name: 'error-handler'},
         {name: 'logger'},
         {name: 'router', urls: urls}
     ]
-})
+};
+
+genji.web.startServer(settings);
