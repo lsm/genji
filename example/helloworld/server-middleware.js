190b675be97b420f77cc395ee30881877c28ba14
Senmiao Liu <zir.echo@gmail.com>
2010-12-18 01:27:52 +0800

    update example

--- example/helloworld/server-middleware.js
+++ example/helloworld/server-middleware.js
@@ -3,5 +3,5 @@ var genji = require('../../lib/genji');
 genji.web.startServer({
     host: '127.0.0.1',
     port: '8000',
-    middlewares: [{name: 'helloworld'}]
+    middlewares: {'helloworld': {}}
 })
