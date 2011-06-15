4e2c46f2fb40bd9b0759f4938b950def2b0c4fe5
Senmiao Liu <zir.echo@gmail.com>
2011-06-15 22:20:45 +0800

    require path

---
+++ test/util.test.js
@@ -0,0 +1,9 @@
+var util = require('../lib/genji/util');
+var assert = require('assert');
+
+module.exports = {
+	'test base64 encoding end decoding': function() {
+		assert.equal(util.base64Encode('你好'), '5L2g5aW9');
+		assert.equal(util.base64Decode('5L2g5aW9'), '你好');
+	}
+}
--- test/util/base64.test.js
+++
@@ -1,9 +0,0 @@
-var base64 = require('genji/util').base64;
-var assert = require('assert');
-
-module.exports = {
-    'test base64 encoding end decoding': function() {
-        assert.equal(base64.encode('你好'), '5L2g5aW9');
-        assert.equal(base64.decode('5L2g5aW9'), '你好');
-    }
-}
