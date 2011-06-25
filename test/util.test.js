c9f3cdccb43c979fe4cabbb8280a4bdb0b2e23af
Senmiao Liu <zir.echo@gmail.com>
2011-06-25 22:52:12 +0800

    full test for `util`

--- test/util.test.js
+++ test/util.test.js
@@ -6,5 +6,22 @@ module.exports = {
   'test base64 encoding end decoding': function() {
     assert.equal(util.base64Encode('你好'), '5L2g5aW9');
     assert.equal(util.base64Decode('5L2g5aW9'), '你好');
+  },
+
+  'test util#toArray': function() {
+    var obj = {};
+    (function() {
+      assert.eql([1, '2', obj], util.toArray(arguments));
+    })(1, '2', obj);
+  },
+
+  'test util#extend': function() {
+    var obj = {'a': 1};
+    var t = new Date;
+    var extended = util.extend(obj, {b: '2', c: t});
+    assert.eql(obj, extended);
+    assert.eql(obj.a, 1);
+    assert.eql(obj.b, '2');
+    assert.eql(t, extended.c);
   }
-}
+};
