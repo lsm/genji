af064d2296a8ae1c0d2e4cdb870e2a2ada3d4d18
Senmiao Liu <zir.echo@gmail.com>
2011-07-01 20:33:04 +0800

    let `extends` takes unlimited arguments

--- test/util.test.js
+++ test/util.test.js
@@ -13,10 +13,12 @@ module.exports = {
   'test util#extend': function() {
     var obj = {'a': 1};
     var t = new Date;
-    var extended = util.extend(obj, {b: '2', c: t});
+    var extended = util.extend(obj, {b: '2', c: t}, {b: 4, e: 'e', f: false});
     assert.eql(obj, extended);
     assert.eql(obj.a, 1);
-    assert.eql(obj.b, '2');
+    assert.eql(obj.b, '4');
     assert.eql(t, extended.c);
+    assert.eql('e', extended.e);
+    assert.eql(false, extended.f);
   }
 };
