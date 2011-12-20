e7a23dc0d1793121d5ca2905844856c7f56254c4
Senmiao Liu <zir.echo@gmail.com>
2011-12-21 01:29:23 +0800

    benchmark by benchmarkjs

--- benchmark/base.js
+++ benchmark/base.js
@@ -2,88 +2,123 @@
 var genji = require('../lib/genji').short();
 var Base = genji.Base;
 var sys = require('sys'),
-times = 500000;
-function bm(label, fn) {
-    var start = +new Date
-    fn()
-    sys.puts('  ' + label + ' : ' + (+new Date - start) + ' ms')
-}
+  times = 500000;
 
-sys.puts('\n  running ' + times + ' times\n')
-sys.puts('\n  class definition \n')
-bm('prototype', function(){
-    var n = times
-    while (n--) {
-        function User(name) {
-            this.name = name.trim()
-        }
-        function Admin(name) {
-            User.call(this, name)
-        }
+var Benchmark = require('benchmark');
+
+console.log('Testing class definition: ');
+var suite = new Benchmark.Suite;
+// add tests
+suite.add('prototype', function() {
+  var User = function(name) {
+    if (name && typeof name === 'string') {
+      this.name = name.trim();
     }
+  };
+  User.prototype.auth = function(username, password) {
+  };
+  var Admin = function(name) {
+    User.call(this, name);
+  };
+  Admin.prototype = new User;
+  Admin.prototype.constructor = Admin;
 })
-
-bm('sys.inherits()', function(){
-    var n = times
-    while (n--) {
-        function User(name) {
-            this.name = name.trim()
-        }
-        function Admin(name) {
-            User.call(this, name)
-        }
-        sys.inherits(Admin, User)
-    }
-})
-
-bm('Base', function(){
-    var n = times
-    while (n--) {
-        var User = Base(function(name) {
-            this.name = name.trim()
-        });
-        var Admin = User({
-            init: function(name) {
-                this._super(name);
-            }
-        });
-    }
-})
-
-sys.puts('\n  instance creation \n')
-
-bm('prototype', function(){
-    var n = times
-    function User(name) {
-        this.name = name.trim()
-    }
-    function Admin(name) {
-        User.call(this, name)
-    }
-    while (n--) new Admin('tj')
-})
-
-bm('sys.inherits()', function(){
-    var n = times
-    function User(name) {
-        this.name = name.trim()
-    }
-    function Admin(name) {
-        User.call(this, name)
-    }
-    sys.inherits(Admin, User)
-    while (n--) new Admin('tj')
-})
-
-bm('Base', function(){
-    var n = times
+  .add('sys#inherits', function() {
+    var User = function(name) {
+      if (name && typeof name === 'string') {
+        this.name = name.trim();
+      }
+    };
+    User.prototype.auth = function(username, password) {
+    };
+    var Admin = function(name) {
+      User.call(this, name);
+    };
+    sys.inherits(Admin, User);
+  })
+  .add('genji#base', function() {
     var User = Base(function(name) {
-        this.name = name.trim()
+      if (name && typeof name === 'string') {
+        this.name = name.trim();
+      }
+    }, {
+      auth: function(username, password) {
+      }
     });
     var Admin = User({
-        init: function(name) {
-            this._super(name);
-        }
+      init: function(name) {
+        this._super(name);
+      }
     });
-    while (n--) new Admin('tj')
+  })
+// add listeners
+  .on('cycle', function(event, bench) {
+    console.log(String(bench));
+  })
+  .on('complete', function() {
+    console.log('Fastest is ' + this.filter('fastest').pluck('name'));
+  })
+// run async
+  .run({ 'async': false });
+
+
+console.log('Testing class initialization: ');
+var suite2 = new Benchmark.Suite;
+// prototype
+var UserPrototype = function(name) {
+  if (name && typeof name === 'string') {
+    this.name = name.trim();
+  }
+};
+UserPrototype.prototype.auth = function(username, password) {
+};
+var AdminPrototype = function(name) {
+  UserPrototype.call(this, name);
+};
+AdminPrototype.prototype = new UserPrototype;
+AdminPrototype.prototype.constructor = AdminPrototype;
+// sys#inherits
+var UserSys = function(name) {
+  if (name && typeof name === 'string') {
+    this.name = name.trim();
+  }
+};
+UserSys.prototype.auth = function(username, password) {
+};
+var AdminSys = function(name) {
+  UserSys.call(this, name);
+};
+sys.inherits(AdminSys, UserSys);
+// genji#base
+var UserBase = Base(function(name) {
+  if (name && typeof name === 'string') {
+    this.name = name.trim();
+  }
+}, {
+  auth: function(username, password) {
+  }
+});
+var AdminBase = UserBase({
+  init: function(name) {
+    this._super(name);
+  }
+});
+// add tests
+suite2.add('prototype', function() {
+  new AdminPrototype('zir');
 })
+  .add('sys#inherits', function() {
+    new AdminSys('zir');
+  })
+  .add('genji#base', function() {
+    new AdminBase('zir');
+  })
+// add listeners
+  .on('cycle', function(event, bench) {
+    console.log(String(bench));
+  })
+  .on('complete', function() {
+    console.log('Fastest is ' + this.filter('fastest').pluck('name'));
+  })
+// run async
+  .run({ 'async': false });
