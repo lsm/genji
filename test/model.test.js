a5fe993412b60bf341f41ee637f813ca84ea9ec9
Senmiao Liu <zir.echo@gmail.com>
2013-03-21 23:51:44 +0800

    Rewrite "Model" using "Klass" Update test

--- test/model.test.js
+++ test/model.test.js
@@ -4,11 +4,12 @@ var Model = genji.Model;
 
 exports['test model definition'] = function () {
 
-  var Post = Model('Post', {
+  var Post = Model({
+    name: 'Post',
     fields: {
       id: 'number',
       title: 'string',
-      content:function (content) {
+      content: function (content) {
         if (typeof content !== 'string') {
           return 'content should be string';
         } else if (content.length <= 20) {
@@ -17,45 +18,49 @@ exports['test model definition'] = function () {
       }
     },
     aliases: {id: '_id'},
-    setTitle:function (title) {
+    setTitle: function (title) {
       return title.toUpperCase();
     },
-    getId:function (id) {
+    getId: function (id) {
       return 'postId_' + id;
-    }
-  }, {STATUS_DRAFT: 0});
+    },
+    STATUS_DRAFT: 0
+  });
 
   var post = new Post({
     id: 10,
     title: 'example post title',
     content: 'invalid content'
   });
-  
+
   assert.eql(post.isValid(), false);
-  assert.eql(post.attr('id'), 'postId_10');
+  assert.eql(post.get('id'), 'postId_10');
+  assert.eql(post.get('title'), 'EXAMPLE POST TITLE');
   var postDoc = post.toData();
   assert.eql(postDoc, false);
-  assert.eql(Post.STATUS_DRAFT, 0);
+  assert.eql(post.STATUS_DRAFT, 0);
   var invalidFields = post.getInvalidFields();
   assert.eql(invalidFields.content.error, 'content should be longer than 20 characters');
   assert.eql(invalidFields.content.value, 'invalid content');
 
-  var PostExtended = Post('PostExtended', {
+  var PostExtended = Post({
+    name: 'PostExtended',
     requires: ['title', 'content'],
     fields: {
-      content:function (content) {
+      content: function (content) {
         if (typeof content !== 'string') {
           return 'content should be string';
         } else if (content.length <= 10) {
           return 'content should be longer than 10 characters';
         }
       },
-      author:function (author) {
+      author: function (author) {
         return author !== 'post author';
       }
     },
-    aliases: {author: 'postAuthor'}
-  }, {STATUS_PUBLISHED: 1});
+    aliases: {author: 'postAuthor'},
+    STATUS_PUBLISHED: 1
+  });
 
   var postExtended = new PostExtended({
     id: 20,
@@ -64,12 +69,12 @@ exports['test model definition'] = function () {
     author: 'post author'
   });
 
-  assert.eql(PostExtended.STATUS_DRAFT, 0);
-  assert.eql(PostExtended.STATUS_PUBLISHED, 1);
+  assert.eql(postExtended.STATUS_DRAFT, 0);
+  assert.eql(postExtended.STATUS_PUBLISHED, 1);
   assert.eql(postExtended.isValid(), true);
-  assert.eql(postExtended.attr('content'), 'is a valid post content');
-  postExtended.attr('content', 'another valid post content')
-  assert.eql(postExtended.attr('content'), 'another valid post content');
+  assert.eql(postExtended.get('content'), 'is a valid post content');
+  postExtended.set('content', 'another valid post content');
+  assert.eql(postExtended.get('content'), 'another valid post content');
   assert.eql(postExtended.changed(), {'content': 'another valid post content'});
 
   var postExtendedDoc = postExtended.toDoc();
@@ -105,5 +110,5 @@ exports['test model definition'] = function () {
     content: 'is a valid post content',
     postAuthor: 'post author'
   });
-  assert.eql(postExtended2.attr('author'), 'post author');
+  assert.eql(postExtended2.get('author'), 'post author');
 };
