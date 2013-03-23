c3a3489ebe4a9121b560cde4a6ab40c08f5b4fb1
Senmiao Liu <zir.echo@gmail.com>
2013-03-24 01:32:32 +0800

    More tests to make sure sub-class will not affect each other

--- test/base.test.js
+++ test/base.test.js
@@ -190,11 +190,21 @@ module.exports = {
     var Subclass = Class({
       getName: function () {
         return 'Subclass: ' + this.name;
+      },
+
+      getKey: function () {
+        return 'Subclass.getKey';
       }
     });
 
     var Class2 = Class(function (name) {
       this.name = 'Class2:' + name;
+    });
+
+    var Class3 = Class({
+      getAge: function () {
+        return 18;
+      }
     });
 
     var cls = new Class('john');
@@ -204,15 +214,21 @@ module.exports = {
     assert.equal(false, cls instanceof Klass);
     assert.equal(true, cls instanceof Class);
     assert.equal('john', cls.getName());
+    assert.equal(undefined, cls.getKey);
+    assert.equal(undefined, cls.getAge);
 
     assert.equal(false, cls2 instanceof Klass);
     assert.equal(true, cls2 instanceof Class);
     assert.equal(true, cls2 instanceof Class2);
     assert.equal('Class2:john', cls2.getName());
+    assert.equal(undefined, cls2.getKey);
+    assert.equal(undefined, cls2.getAge);
 
     assert.equal(false, subclass instanceof Klass);
     assert.equal(true, subclass instanceof Class);
     assert.equal(true, subclass instanceof Subclass);
     assert.equal('Subclass: mike', subclass.getName());
+
+    assert.equal(undefined, Class3.prototype.getKey);
   }
 };
