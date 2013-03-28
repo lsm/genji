d603d7f1e39e7349646e4da78f73a6ba5c7c1c2d
Senmiao Liu <zir.echo@gmail.com>
2013-03-28 23:03:57 +0800

    Merge plugin tests.

--- test/plugin.test.js
+++ test/plugin.test.js
@@ -141,4 +141,84 @@ describe('Plugin', function () {
         .expect(404, "Content not found: /not/existent/url", done);
     });
   });
+
+  describe('.securecookie', function () {
+    var cookie = genji.cookie;
+
+    // options for `secure-cookie` middleware
+    var secureCookieOptions = {
+      cookieName: '_testSecure',
+      secureKey: 'cipher-key',
+      serverKey: 'hmac-key',
+      cookiePath: '/test',
+      cookieDomain: 'test.com'
+    };
+    // input data
+    var now = new Date();
+    var userData = {
+      cookieId: '12345',
+      cookieExpires: new Date((new Date(now.toString())).getTime() + 3600 * 24 * 3 * 1000),
+      cookieData: {a: 1}
+    };
+
+    var testCookiePlugin = {
+      name: 'TestCookie',
+      attach: function () {
+        return function (req, res, go) {
+          if (req.url === '/sign') {
+            var parsedData = JSON.parse(req.headers.data);
+            var cookieOptions = {
+              path: '/test',
+              domain: 'test.com'
+            };
+            var cookie = [parsedData.cookieId, new Date(parsedData.cookieExpires), parsedData.cookieData, cookieOptions];
+            this.setCookie('key', 'value', {expires: new Date(parsedData.cookieExpires)});
+            this.writeHead(200, {'_testSecure': cookie});
+            this.end();
+          } else if (req.url === '/verify') {
+            var session = this.session;
+            assert.equal(session.cookieId, userData.cookieId);
+            assert.deepEqual(new Date(session.cookieExpires), userData.cookieExpires);
+            assert.equal(session.cookieData.a, userData.cookieData.a);
+            this.writeHead(200, {data: JSON.stringify(session)});
+            this.end();
+          }
+        };
+      }
+    };
+
+    it('should be able to sign and verify cookies', function (done) {
+      var site = genji.site();
+      site.use('securecookie', secureCookieOptions);
+      site.use('cookie');
+      site.use(testCookiePlugin);
+      var server = http.createServer();
+      site.start(server);
+
+      request(server)
+        .get('/sign')
+        .set('data', JSON.stringify(userData))
+        .end(function (err, res) {
+          var cookies = res.headers['set-cookie'];
+          var normalCookie = cookie.parse(cookies[0]);
+          assert.equal('value', normalCookie.key);
+          var site = genji.site();
+          site.use('securecookie', secureCookieOptions);
+          site.use(testCookiePlugin);
+          var server = http.createServer();
+          site.start(server);
+          request(server)
+            .get('/verify')
+            .set('Cookie', cookies[1])
+            .end(function (err, res) {
+              var parsedData = JSON.parse(res.headers.data);
+              assert.equal(parsedData.cookieId, userData.cookieId);
+              assert.deepEqual(new Date(parsedData.cookieExpires), userData.cookieExpires);
+              assert.equal(parsedData.cookieData.a, userData.cookieData.a);
+              done();
+            });
+        });
+    });
+  });
+
 });
