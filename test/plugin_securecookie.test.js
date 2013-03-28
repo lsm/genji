1024be137c80ada7d001ce0b790de2e0cc5f0e8b
Senmiao Liu <zir.echo@gmail.com>
2013-03-28 21:31:15 +0800

    Use context to set cookie, update test.

--- test/plugin_securecookie.test.js
+++ test/plugin_securecookie.test.js
@@ -2,12 +2,23 @@ var genji = require('../index');
 var assert = require('assert');
 var http = require('http');
 var request = require('supertest');
+var cookie = genji.cookie;
 
 // options for `secure-cookie` middleware
-var secureCookieOptions = {cookieName: '_testSecure', path: '../../lib/middleware', secureKey: 'cipher-key', serverKey: 'hmac-key'};
+var secureCookieOptions = {
+  cookieName: '_testSecure',
+  secureKey: 'cipher-key',
+  serverKey: 'hmac-key',
+  cookiePath: '/test',
+  cookieDomain: 'test.com'
+};
 // input data
 var now = new Date();
-var userData = {cookieId: '12345', cookieExpires: new Date((new Date(now.toString())).getTime() + 3600 * 24 * 3 * 1000), cookieData: {a: 1}};
+var userData = {
+  cookieId: '12345',
+  cookieExpires: new Date((new Date(now.toString())).getTime() + 3600 * 24 * 3 * 1000),
+  cookieData: {a: 1}
+};
 
 var testCookiePlugin = {
   name: 'TestCookie',
@@ -15,7 +26,12 @@ var testCookiePlugin = {
     return function (req, res, go) {
       if (req.url === '/sign') {
         var parsedData = JSON.parse(req.headers.data);
-        var cookie = [parsedData.cookieId, new Date(parsedData.cookieExpires), parsedData.cookieData];
+        var cookieOptions = {
+          path: '/test',
+          domain: 'test.com'
+        };
+        var cookie = [parsedData.cookieId, new Date(parsedData.cookieExpires), parsedData.cookieData, cookieOptions];
+        this.setCookie('key', 'value', {expires: new Date(parsedData.cookieExpires)});
         this.writeHead(200, {'_testSecure': cookie});
         this.end();
       } else if (req.url === '/verify') {
@@ -35,6 +51,7 @@ describe('Plugin', function () {
     it('should be able to sign and verify cookies', function (done) {
       var site = genji.site();
       site.use('securecookie', secureCookieOptions);
+      site.use('cookie');
       site.use(testCookiePlugin);
       var server = http.createServer();
       site.start(server);
@@ -43,7 +60,9 @@ describe('Plugin', function () {
         .get('/sign')
         .set('data', JSON.stringify(userData))
         .end(function (err, res) {
-          var signedCookie = res.headers['set-cookie'];
+          var cookies = res.headers['set-cookie'];
+          var normalCookie = cookie.parse(cookies[0]);
+          assert.equal('value', normalCookie.key);
           var site = genji.site();
           site.use('securecookie', secureCookieOptions);
           site.use(testCookiePlugin);
@@ -51,7 +70,7 @@ describe('Plugin', function () {
           site.start(server);
           request(server)
             .get('/verify')
-            .set('Cookie', signedCookie[0])
+            .set('Cookie', cookies[1])
             .end(function (err, res) {
               var parsedData = JSON.parse(res.headers.data);
               assert.equal(parsedData.cookieId, userData.cookieId);
