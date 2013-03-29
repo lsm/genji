b397291da2a32bc82dc7e67233ea0fdbcff0817a
Senmiao Liu <zir.echo@gmail.com>
2013-03-29 23:21:56 +0800

    Update docs and reports.

--- plato/files/lib_genji_js/report.js
+++ plato/files/lib_genji_js/report.js
@@ -10,39 +10,39 @@ __report = {
       "line": 11,
       "complexity": {
         "sloc": {
-          "physical": 57,
-          "logical": 28
+          "physical": 58,
+          "logical": 29
         },
         "cyclomatic": 2,
         "halstead": {
           "operators": {
             "distinct": 10,
-            "total": 65,
+            "total": 67,
             "identifiers": [
               "__stripped__"
             ]
           },
           "operands": {
-            "distinct": 33,
-            "total": 96,
+            "distinct": 34,
+            "total": 100,
             "identifiers": [
               "__stripped__"
             ]
           },
-          "length": 161,
-          "vocabulary": 43,
-          "difficulty": 14.545454545454547,
-          "volume": 873.6286255070378,
-          "effort": 12707.32546192055,
-          "bugs": 0.29120954183567926,
-          "time": 705.9625256622528
+          "length": 167,
+          "vocabulary": 44,
+          "difficulty": 14.705882352941178,
+          "volume": 911.7250803124286,
+          "effort": 13407.721769300422,
+          "bugs": 0.3039083601041429,
+          "time": 744.8734316278012
         }
       }
     },
     "functions": [
       {
         "name": "route",
-        "line": 54,
+        "line": 55,
         "complexity": {
           "sloc": {
             "physical": 4,
@@ -76,7 +76,7 @@ __report = {
       },
       {
         "name": "site",
-        "line": 65,
+        "line": 66,
         "complexity": {
           "sloc": {
             "physical": 3,
